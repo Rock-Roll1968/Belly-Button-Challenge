409d11225d4712cd5fcf8d54a6660574d478f738
Rock-Roll1968 <ernesto.v.garcia@outlook.com>
2024-10-28 12:46:09 -0500

    Completed

--- static/js/app.js
+++ static/js/app.js
@@ -4,7 +4,7 @@ let url = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
 const optionChanged = async id => {
     let { names, metadata, samples } = await d3.json(url);
 
-// mames
+// names
     if(id == undefined) {
       names.forEach(name => {
         d3.select("select").append("option").text(name)
@@ -34,7 +34,6 @@ const optionChanged = async id => {
     combinedData.sort((a, b) => b.value - a.value);
     combinedData = combinedData.slice(0, 10).reverse();
     
-    
     var data = [
       {
         x: combinedData.map(d => d.value),
@@ -45,52 +44,33 @@ const optionChanged = async id => {
        }];
     
     Plotly.newPlot('bar', data);
-};
-
-// Bubble chart
-
- let samples = [
-    {
-      id: 1,
-      otu_ids: [1167],
-      sample_values: [165],
-      otu_labels: []
-    },
-  ]; 
-
- function createBubbleChart(id) {
-
-  let {otu_ids, sample_values, otu_labels } = samples.find(obj => obj.id == id);
-  
-
-  var trace = {
-    x: otu_ids,
-    y: sample_values,
-    text: otu_labels,
-    mode: 'markers',
-    marker: {
-      size: sample_values,
-      color: otu_ids,
-      colorscale: 'Earth',
-      opacity: 0.6
+    
+// Bubble chart     
+    let trace = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: 'Earth',
+        opacity: 0.6
+      }
     }
-  };
 
-  var data = [trace];
+    let data1 = [trace];
 
-  var layout = {
-    title: 'Bubble Chart for Each Sample',
-    xaxis: { title: 'OTU ID' },
-    yaxis: { title: 'Sample Value' },
-    showlegend: false,
-    height: 600,
-    width: 1000
-  };
+    let layout = {
+      title: 'Bubble Chart',
+      xaxis: { title: 'OTU IDs' },
+      yaxis: { title: 'Sample Values'},
+      height: 600,
+      width: 1000
+    };
 
-  Plotly.newPlot('bubble', data, layout)
- }
-
-createBubbleChart(1);
+    Plotly.newPlot('bubble', data1);
+};
 
 let init = optionChanged;
 
